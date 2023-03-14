ef05889df67e52d8045790eb2f3be3b0434a427c
astashenkov <ya.astashenkov@gmail.com>
2023-03-14 23:35:31 +0300

    Add staticmethod into Triangle class

--- src/Triangle.py
+++ src/Triangle.py
@@ -3,14 +3,9 @@ import math
 from Figure import Figure
 
 
-def check_is_can_create_triangle(a_side: int, b_side: int, c_side: int):
-    if not (a_side + b_side > c_side and a_side + c_side > b_side and c_side + b_side > a_side):
-        raise ValueError(f'Unable to create a triangle with sides: {a_side}, {b_side}, {c_side}')
-
-
 class Triangle(Figure):
     def __init__(self, a_side: int, b_side: int, c_side: int):
-        check_is_can_create_triangle(a_side, b_side, c_side)
+        Triangle.check_is_can_create_triangle(a_side, b_side, c_side)
         self.name = 'Triangle'
         self.a_side = a_side
         self.b_side = b_side
@@ -28,3 +23,8 @@ class Triangle(Figure):
     @property
     def perimeter(self) -> int:
         return self.a_side + self.b_side + self.c_side
+
+    @staticmethod
+    def check_is_can_create_triangle(a_side: int, b_side: int, c_side: int):
+        if not (a_side + b_side > c_side and a_side + c_side > b_side and c_side + b_side > a_side):
+            raise ValueError(f'Unable to create a triangle with sides: {a_side}, {b_side}, {c_side}')
