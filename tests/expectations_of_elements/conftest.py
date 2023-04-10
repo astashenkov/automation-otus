90021e78cc25e743d467d1d199670fe7d3118aec
astashenkov <ya.astashenkov@gmail.com>
2023-04-10 23:29:24 +0300

    Update conftest file

--- tests/expectations_of_elements/conftest.py
+++ tests/expectations_of_elements/conftest.py
@@ -1,35 +1,53 @@
 import pytest
 from selenium import webdriver
-from selenium.webdriver.firefox.webdriver import WebDriver
+from selenium.webdriver.chrome.options import Options as ChromeOptions
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from selenium.webdriver.safari.options import Options as SafariOptions
+from selenium.webdriver.safari.service import Service as SafariService
+from selenium.webdriver.edge.options import Options as EdgeOptions
+from selenium.webdriver.edge.service import Service as EdgeService
 
 
 def pytest_addoption(parser):
     parser.addoption(
         '--browser',
         default='chrome',
-        help='Choose browser: chrome, firefox, opera, safari, edge, yandex'
-    )
+        help='Choose browser: Chrome, FireFox, Safari or Edge.')
 
 
 @pytest.fixture()
-def browser(request) -> WebDriver:
+def browser(request):
+    CHROMEDRIVER_PATH = 'drivers/chromedriver'
+    FIREFOXDRIVER_PATH = 'drivers/geckodriver'
+    EDGEDRIVER_PATH = 'drivers/'
     browser_name = request.config.getoption('browser')
 
     match browser_name:
         case 'chrome':
-            driver: WebDriver = webdriver.Chrome('drivers/chromedriver')
+            options = ChromeOptions()
+            options.page_load_strategy = 'normal'
+            service = ChromeService(executable_path=CHROMEDRIVER_PATH)
+            driver = webdriver.Chrome(service=service, options=options)
         case 'firefox':
-            driver: WebDriver = webdriver.Firefox()
-        case 'opera':
-            driver: WebDriver = webdriver.Opera()
+            options = FirefoxOptions()
+            options.page_load_strategy = 'normal'
+            service = FirefoxService(executable_path=FIREFOXDRIVER_PATH)
+            driver = webdriver.Firefox(service=service, options=options)
         case 'safari':
-            driver: WebDriver = webdriver.Safari()
+            options = SafariOptions()
+            options.page_load_strategy = 'normal'
+            service = SafariService()
+            driver = webdriver.Safari(service=service, options=options)
         case 'edge':
-            driver: WebDriver = webdriver.Edge()
-        case 'yandex':
-            driver: WebDriver = webdriver.Chrome()
+            options = EdgeOptions()
+            options.page_load_strategy = 'normal'
+            service = EdgeService(executable_path=EDGEDRIVER_PATH)
+            driver = webdriver.Edge(service=service, options=options)
         case _:
             raise ValueError(f'Unsupported browser: {browser_name}')
 
+    driver.maximize_window()
     yield driver
     driver.quit()
--- tests/expectations_of_elements/tests/test_mock.py
+++ tests/expectations_of_elements/tests/test_mock.py
@@ -4,4 +4,4 @@ import time
 class TestOpenCart:
     def test_mock_open_browser(self, browser):
         browser.get('https://www.google.com')
-        time.sleep(3)
+        time.sleep(1)
