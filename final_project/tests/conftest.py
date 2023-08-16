8311c1d898346a51c5cae82dccd6e7ad6f392b2f
astashenkov <ya.astashenkov@gmail.com>
2023-08-17 00:01:28 +0300

    FINAL PROJECT: Update conftest file

--- final_project/tests/conftest.py
+++ final_project/tests/conftest.py
@@ -1,40 +1,69 @@
 import pytest
 
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options as ChromeOptions
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
+from selenium.webdriver.edge.options import Options as EdgeOptions
+from selenium.webdriver.safari.options import Options as SafariOptions
 
 
 def pytest_addoption(parser):
     parser.addoption(
         '--browser',
         default='chrome',
-        help='Choose browser: Chrome, FireFox, Safari, Opera or Edge.'
+        help='Choose browser: Chrome, FireFox, Safari or Edge'
     )
     parser.addoption(
         '--url',
         default='https://www.automationexercise.com/',
         help='Choose base URL'
     )
+    parser.addoption(
+        "--headless",
+        action='store_true',
+        help='Selecting the browser working mode'
+    )
 
 
 @pytest.fixture()
 def driver(request):
-    browser_name = request.config.getoption('browser').lower()
+    browser_name = request.config.getoption('--browser')
+    headless = request.config.getoption('--headless')
+
+    service = Service()
 
     match browser_name:
         case 'chrome':
-            driver = webdriver.Chrome()
+            options = ChromeOptions()
+            if headless:
+                options.add_argument('-headless')
+            driver = webdriver.Chrome(service=service, options=options)
         case 'firefox':
-            driver = webdriver.Firefox()
+            options = FirefoxOptions()
+            if headless:
+                options.add_argument('-headless')
+            driver = webdriver.Firefox(service=service, options=options)
         case 'safari':
-            driver = webdriver.Safari()
+            options = SafariOptions()
+            if headless:
+                options.add_argument('-headless')
+            driver = webdriver.Safari(service=service, options=options)
         case 'edge':
-            driver = webdriver.Edge()
+            options = EdgeOptions()
+            if headless:
+                options.add_argument('-headless')
+            driver = webdriver.Edge(service=service, options=options)
         case _:
             raise ValueError(f'Unsupported browser: {browser_name}')
 
-    driver.browser_base_url = request.config.getoption('url')
     driver.maximize_window()
 
     yield driver
 
     driver.quit()
+
+
+@pytest.fixture()
+def base_url(request):
+    return request.config.getoption("--base_url")
